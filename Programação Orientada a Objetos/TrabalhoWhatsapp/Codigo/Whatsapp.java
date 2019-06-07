2ad1ed8ca4be5780563a62d89bd2b84bcf21568c
vitorhartmann <32484028+vitorhartmann@users.noreply.github.com>
2019-06-07 19:43:00 -0300

    Alterado 07/06

--- Programação Orientada a Objetos/TrabalhoWhatsapp/Codigo/Whatsapp.java
+++ Programação Orientada a Objetos/TrabalhoWhatsapp/Codigo/Whatsapp.java
@@ -1,7 +1,28 @@
-public class Whatsapp {
+import java.util.Scanner;
+
+class Usuario{
+    public static void main(String[] args){
+
+    String nome;
+    String cpf;
+
+    Usuario meuUsuario;
+    meuUsuario = new Usuario();
+    
+    meuUsuario.nome = ("Usuario");
+    meuUsuario.cpf = ("111.111.111-11");
+    
+    System.out.println("Cpf: " + meuUsuario.nome);
+    System.out.println("Cpf: " + meuUsuario.cpf);
+    
+
+    }
+}
+
+
+class Whatsapp {
 
     public static void main(String[] args){
-// INICIO DO CÓDIGO
 
 
 
@@ -17,6 +38,9 @@ public class Whatsapp {
 
 
 
-// FIM DO CÓDIGO
+
+
+
 }
 }
+
